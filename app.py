741c641bcfb0f236f7726584216b156ed3aa03d8
TechnoTalksDev <technotalksyt101@gmail.com>
2022-04-14 21:17:18 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index(): 
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("generic.html")
+
+if __name__ == "__main__":
+    os.environ["FLASK_ENV"] = "development"
+    app.run(host="0.0.0.0", port=80, debug=True)
